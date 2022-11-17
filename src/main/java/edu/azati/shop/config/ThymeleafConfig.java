b081eab7869ad297bde1a810ae23ce81f278cded
Zhanna <zpuras@mail.ru>
2022-11-17 16:20:59 +0300

    ThymeleafConfig

---
+++ src/main/java/edu/azati/shop/config/ThymeleafConfig.java
@@ -0,0 +1,13 @@
+package edu.azati.shop.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.thymeleaf.extras.springsecurity4.dialect.SpringSecurityDialect;
+
+@Configuration
+public class ThymeleafConfig {
+    @Bean
+    public SpringSecurityDialect springSecurityDialect(){
+        return new SpringSecurityDialect();
+    }
+}
